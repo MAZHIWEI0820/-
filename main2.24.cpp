b3dadf0a350d12b6184e3f56ddcb866ddce3d917
MAZHIWEI0820 <2537919389@qq.com>
2019-03-19 21:52:43 +0800

    马志伟2.24作业

---
+++ main2.24.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int number1=0;
+
+    cout<<"输入的数是:";
+    cin>>number1;
+
+    if(number1%2==0)
+        cout<<"number1 is 偶数"<<endl;
+    else
+        cout<<"number1 is 奇数"<<endl;
+}
